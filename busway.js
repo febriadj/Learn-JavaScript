a4de80e39bb22cc665330fbfc27e30793a03f10f
i'mfebri <73212374+febriadj@users.noreply.github.com>
2020-12-05 15:03:19 +0700

    initial commit

--- busway.js
+++ busway.js
@@ -1,4 +1,4 @@
-// Membuat Program Pengelolaan Busway.
+// membuat program pengelolaan busway.
 function busway(namaSupir, penumpang, jumlahPenumpang, saldo){
   let obj = {};
   obj.namaSupir       = namaSupir;
@@ -6,18 +6,23 @@ function busway(namaSupir, penumpang, jumlahPenumpang, saldo){
   obj.jumlahPenumpang = jumlahPenumpang;
   obj.saldo           = saldo;
 
+  // kondisi saat penumpang naik  
   obj.penumpangNaik = function(namaPenumpang){
     if( obj.penumpang.length == 0 ){
-      obj.penumpang.push(namaPenumpang);
+      obj.penumpang.push(namaPenumpang); // masukkan nama penumpang kedalam object penumpang
       return penumpang;
     } else {
       for( let i = 0; i < obj.penumpang.length; i++ ){
+        // kondisi saat nama penumpang sudah ada didalam busway
         if( obj.penumpang == namaPenumpang ){
           console.log(`Penumpang dengan nama ${namaPenumpang} Sudah ada didalam busway`);
           return penumpang;
+          // kondisi jika kursi penumpang kosong
+          // masukan nama penumpang ke kursi kosong terlebih dahulu
         } else if ( obj.penumpang[i] == undefined ){
           obj.penumpang[i] = namaPenumpang;
           return penumpang;
+          // memasukkan nama penumpang ke kursi berikutnya
         } else if ( i == obj.penumpang.length - 1 ){
           obj.penumpang.push(namaPenumpang);
           return penumpang;
@@ -25,18 +30,26 @@ function busway(namaSupir, penumpang, jumlahPenumpang, saldo){
       }
     }
   }
-
+  
+  // kondisi saat penumpang turun
   obj.penumpangTurun = function(namaPenumpang, bayar){
+    // beri tahu saat menurunkan penumpang
+    // jika penumpang didalam busway masih kosong
     if( obj.penumpang.length == 0 ){
       console.log(`Busway saat ini sedang kosong, tidak ada yang bisa dikeluarkan`);
       return penumpang;
     } else {
       for( let i = 0; i < obj.penumpang.length; i++ ){
+        // kondisi saat nama penumpang sama dengan yang ada didalam busway
+        // turunkan penumpang
+        // object saldo diisi dengan bayaran dari penumpang
+        // object jumlah penumpang + 1 setiap penumpang turun
         if( obj.penumpang[i] == namaPenumpang ){
           obj.penumpang[i] = undefined;
           obj.saldo += bayar;
           obj.jumlahPenumpang += i + 1;
           return penumpang;
+          // kondisi saat nama penumpang tidak ada didalam busway
         } else if ( i == obj.penumpang.length - 1){
           console.log(`Tidak ada nama ${namaPenumpang} didalam busway`);
           return penumpang;
@@ -44,7 +57,10 @@ function busway(namaSupir, penumpang, jumlahPenumpang, saldo){
       }
     }
   }
+  // kembalikan nilai obj
   return obj;
 }
 
-let buswayBlue = new busway('El', [], 0, 0);
+// pemanggilan function
+const buswayBlue = new busway('nama supir1', [], 0, 0);
+const buswayOrange = new busway('nama supir2', [], 0, 0);
